89b0b8cb6c481d1a5958c216754cbaa8776fe932
Mikola <matiko.mikola@gmail.com>
2021-09-09 10:30:52 +0300

    I created validation.js and api.js,where moved the functions validation and axios.Exported and imported into sign_up.js

---
+++ src/Pages/api.js
@@ -0,0 +1,16 @@
+import axios from "axios"
+import validation from "./validation";
+
+// const ApiAxios = async()=> {
+//     await axios({
+//         url: 'https://app-social-network-1.herokuapp.com/auth/register',
+//         data: {
+//             name: name,
+//             email: email,
+//             password: password
+//         },
+//         method: 'post'
+//     },).then(rs => console.log(rs)).catch(e => console.log('error :', e))
+// }
+
+// export default ApiAxios;
---
+++ src/Pages/validation.js
@@ -0,0 +1,37 @@
+import ApiAxios from "./api";
+
+const Validation = (name, email, password, repeatPassword) => {
+    const re = /\S+@\S+\.\S+/
+    if (name.length <= 5 || name.length >= 15) {
+        return {
+            status: 'Fail',
+            field: 'name',
+            errorMessage: "The name must have min 5 and max 15 letters"
+        }
+    } else if (!re.test(email)) {
+        return {
+            status: 'Fail',
+            field: 'email',
+            errorMessage: "Email is not correct"
+        }
+    } else if (password.length <= 5) {
+        return {
+            status: 'Fail',
+            field: 'password',
+            errorMessage: "Password is not correct,the password must have minimum 5 letters"
+        }
+    } else if (password !== repeatPassword) {
+        return {
+            status: 'Fail',
+            field: 'repeatPassword',
+            errorMessage: "Password and confirm password must be the same!"
+        }
+    }
+    return {
+        status: 'Ok',
+        field: null,
+        errorMessage: null,
+    }
+}
+
+export default Validation;
