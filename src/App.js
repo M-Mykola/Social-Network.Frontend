b574b90266067f144ead005ee5916a443c633d20
Mikola <matiko.mikola@gmail.com>
2021-09-21 15:17:17 +0300

    Created error 404 page wuith text.Added to Add.js file ror render error page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import "./App.css";
 import React from "react";
 import {
-    BrowserRouter as Router,Switch,Route
+    BrowserRouter as Router, Switch, Route
 } from "react-router-dom";
 import Home from "./Pages/home"
-import  Login from './Pages/sign_in'
-import  Register from './Pages/sign_up'
+import Login from './Pages/sign_in'
+import Register from './Pages/sign_up'
 import WelcomeSignIn from "./Pages/Welcome-Sign-In-Page";
+import Error from "./Pages/Error404";
+
 function App() {
     return (
         <Router>
             <div className="App">
-            <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/sign-in" component={Login}/>
-                <Route path="/sign-up" component={Register}/>
-                <Route path="/welcome" exact component={WelcomeSignIn}/>
-            </Switch>
+
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/sign-in" component={Login}/>
+                    <Route path="/sign-up" component={Register}/>
+                    <Route path="/welcome"  component={WelcomeSignIn}/>
+                    <Route component={Error} />
+                </Switch>
             </div>
         </Router>
     )
