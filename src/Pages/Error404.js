e2c8fec457e77b1dddd69ffab4932d0f3b241ca6
Mikola <matiko.mikola@gmail.com>
2021-09-21 15:18:05 +0300

    Created error 404 page wuith text.Added to Add.js file ror render error page

---
+++ src/Pages/Error404.js
@@ -0,0 +1,14 @@
+
+function Error() {
+
+    return (
+        <header className="input-container">
+            <h1>
+                404 Error.
+                Page not found
+            </h1>
+        </header>
+    );
+}
+
+export default Error;
